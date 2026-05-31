295ff05614358865a86676db54287a2650dae7fc
Faisal Ahammad <faisalahammad24@gmail.com>
2026-05-31 11:57:46 +0600

    fix(pcp): Phase 1 - add ABSPATH direct-access guards to 7 PHP files per PluginCheck.CodeAnalysis.DirectFileAccess

--- php/class-wp-cli.php
+++ php/class-wp-cli.php
@@ -1,4 +1,8 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 /**
  * Manage co-authors and guest authors.
  *
--- php/integrations/yoast.php
+++ php/integrations/yoast.php
@@ -1,4 +1,8 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 /**
  * The main file for the Yoast integration
  */
